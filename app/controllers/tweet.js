9ad2fc61e37fd9bcd58a26d6464e805792ca0752
David Ferreira <daminufe.ti@gmail.com>
2015-01-14 11:52:49 +0100

    Improvements on add tweet

--- app/controllers/tweet.js
+++ app/controllers/tweet.js
@@ -2,6 +2,8 @@ import Ember from "ember";
 
 var TweetController = {
 	isAdding: false,
+	emailField: null,
+	tweetField: null,
 	actions: {
 		add: function() {
 			this.set('isAdding', true);
@@ -9,7 +11,11 @@ var TweetController = {
 
 		doneAdd: function() {
 			this.set('isAdding', false);
-		}
+		}, 
+
+		tweetLimitReached: function() {
+			this.get('tweetField').length;
+		}.observes('tweetField')
 	}
 };
 
