cb1047a6e02c7d6092b484352653b407e389050e
David Ferreira <daminufe.ti@gmail.com>
2015-01-15 14:08:11 +0100

    Some changes on interface (partial)

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,17 +3,6 @@ import Ember from "ember";
 var ApplicationController = {
 	userName: "David Ferreira",
 	userEmail: "daminufe.ti@gmail.com",
-	searchQuery: '',
-	actions: {
-		searchAction: function() {
-			var query = this.get('searchQuery');
-			console.log(query);
-		},
-
-		searchByEmail: function() {
-
-		}
-	}
 };
 
 export default Ember.ObjectController.extend(ApplicationController);
